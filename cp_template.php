7e1e54ff48616c713cfd04cffb32b5d8767a5dd4
CTADMIN2588 <124482099+CTADMIN2588@users.noreply.github.com>
2024-09-09 19:01:55 +0530

    Update cp_template.php

--- cp_template.php
+++ cp_template.php
@@ -25,7 +25,6 @@
 	<head>
 		<title>Commtrunks</title>
 		<link rel="stylesheet" href="<?php echo $layout; ?>" />
-		<link rel="stylesheet" href="css/default.css">
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Protest+Guerrilla&display=swap" rel="stylesheet">
